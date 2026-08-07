969967e267a0aa834fd9dad0540238d1f1c91601
mojeedadeleke <akinkunmiadeleke84@gmail.com>
2026-08-07 21:02:20 +0100

    added user-data with replacement on change

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -5,6 +5,14 @@ resource "aws_instance" "example" {
   ami           = "ami-048f644e868baa0e8"
   instance_type = "t3.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.xhtml
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+user_data_replace_on_change = true
+
   tags = {
     name = "terraform-example"
   }
