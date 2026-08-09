baee1fdf87d6a0e88d088014adb687e214bb009b
mojeedadeleke <akinkunmiadeleke84@gmail.com>
2026-08-09 01:19:24 +0100

    Auto-Scaling Group and Load Balancer added

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -1,23 +1,26 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_instance" "example" {
-  ami                    = "ami-048f644e868baa0e8"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-  key_name               = aws_key_pair.deployer.key_name
 
-  user_data = <<-EOF
-              #!/bin/bash
-              cd /home/ec2-user
-              echo "Hello World, salaam" > index.html
-              nohup python3 -m http.server ${var.server_port} &
-              EOF
+resource "aws_launch_template" "example" {
+  image_id      = "ami-048f644e868baa0e8"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.deployer.key_name
 
-  user_data_replace_on_change = true
+  network_interfaces {
+    security_groups = [aws_security_group.instance.id]
+  }
 
-  tags = {
-    name = "terraform-example"
+  user_data = base64encode(<<-EOF
+        #!/bin/bash
+        cd /home/ec2-user
+        echo "Hello World, salaam!" > index.html
+        nohup python3 -m http.server ${var.server_port} &
+        EOF
+  )
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -39,18 +42,126 @@ resource "aws_security_group" "instance" {
   }
 }
 
-output "public_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP of the wbe server"
-}
-
 resource "aws_key_pair" "deployer" {
-  key_name    = "ec2-key"
-  public_key  = file("~/.ssh/ec2-key.pub")
+  key_name   = "ec2-key"
+  public_key = file("~/.ssh/ec2-key.pub")
 }
 
 variable "server_port" {
-  description   = "The port the server will use for HTTP requests"
-  type          = number
-  default       = 8080
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
+
+resource "aws_autoscaling_group" "example" {
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  target_group_arns   = [aws_lb_target_group.asg.arn]
+  health_check_type   = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb-sg.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb-sg" {
+  name = "terraform-example-alb-sg"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+resource "aws_lb_target_group" "asg" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the Application load balancer"
+}
+  
